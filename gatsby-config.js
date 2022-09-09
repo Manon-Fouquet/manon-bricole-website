ba2da63003c69039fa53e30fe419ca9bba211503
mfouquet <manon.fouquet@artelys.com>
2022-09-09 10:07:47 +0200

    added styled-components

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,6 +6,7 @@ module.exports = {
   plugins: [
     "gatsby-plugin-image",
     "gatsby-plugin-sharp",
-    `gatsby-plugin-sass`
+    `gatsby-plugin-sass`,
+    `gatsby-plugin-styled-components`
   ],
 };
