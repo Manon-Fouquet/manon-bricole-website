6da8be6e2d8bb869f8047a9f6b4436ecdc8a5edb
mfouquet <manon.fouquet@artelys.com>
2022-09-15 08:46:01 +0200

    setup bootstrap

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -5,3 +5,11 @@
  */
 
 // You can delete this file if you're not using it
+
+// from https://ibrahim-haouari.medium.com/adding-bootstrap-5-to-your-gatsby-project-4d8878162ed7
+// NOT import "https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css"
+//npm install bootstrap@5.0.0-beta1 @popperjs/core@2.6.0 --save
+
+import "@popperjs/core/dist/umd/popper.min"
+import "bootstrap/dist/css/bootstrap.min.css"
+import "bootstrap/dist/js/bootstrap.min"
